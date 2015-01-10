9139055dfc4e4ed675a323c94751dac2909db423
Thallison Morais <thallisonmorais@gmail.com>
2015-01-10 15:21:00 -0200

    Adicionada configurações no module.config

--- config/module.config.php
+++ config/module.config.php
@@ -8,5 +8,19 @@
 namespace MPDFModule;
 
 return array(
-    
+    'view_manager' => array(
+        'strategies' => array(
+            'ViewMpdfStrategy'
+        )
+    ),
+    'service_manager' => array(
+        'shared' => array(
+            'MPDF' => false
+        ),
+        'factories' => array(
+            'MPDF'          => __NAMESPACE__ . '\Service\MpdfService',
+            'ViewMpdfRender' => __NAMESPACE__ . '\Model\ViewMpdfRender',
+            'ViewMpdfStrategy' => __NAMESPACE__ . '\Model\ViewMpdfStrategy',
+        )
+    ),
 );
