f80b25b31abc7b82b2dcb6698a16058bbfa66b4e
Lucas Araujo <lucasaraujo@dcc.ufmg.br>
2015-10-05 14:23:37 -0300

    alterando a pasta do tmp e ttfontdata

--- src/MPDFModule/Service/MpdfService.php
+++ src/MPDFModule/Service/MpdfService.php
@@ -13,7 +13,11 @@ use mPDF;
 
 class MpdfService implements FactoryInterface
 {
-    
+
+    const MPDF_DIR = 'data/mpdf/';
+    const TEMP_DIR = self::MPDF_DIR . 'temp/';
+    const TTFONTDATA_DIR = self::MPDF_DIR . 'ttfontdata/';
+
     /**
      * Cria a instancia do mpdf
      * 
@@ -22,6 +26,19 @@ class MpdfService implements FactoryInterface
      */
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
+        if (!file_exists(self::MPDF_DIR)) {
+            mkdir(self::MPDF_DIR, 0777, true);
+        }
+        if (!file_exists(self::TEMP_DIR)) {
+            mkdir(self::TEMP_DIR, 0777, true);
+        }
+        if (!file_exists(self::TTFONTDATA_DIR)) {
+            mkdir(self::TTFONTDATA_DIR, 0777, true);
+        }
+
+        define("_MPDF_TEMP_PATH", self::TEMP_DIR);
+        define("_MPDF_TTFONTDATAPATH", self::TTFONTDATA_DIR);
         return new mPDF();
+
     }
 }
