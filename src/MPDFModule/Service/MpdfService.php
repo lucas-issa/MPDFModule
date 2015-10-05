8539e7738918111525dfc7e2ee633f04a01798a5
Lucas Araujo <lucasaraujo@dcc.ufmg.br>
2015-10-05 15:05:41 -0300

    Adicionando o path para o jgraph_path

--- src/MPDFModule/Service/MpdfService.php
+++ src/MPDFModule/Service/MpdfService.php
@@ -17,6 +17,7 @@ class MpdfService implements FactoryInterface
     const MPDF_DIR = 'data/mpdf/';
     const TEMP_DIR = self::MPDF_DIR . 'temp/';
     const TTFONTDATA_DIR = self::MPDF_DIR . 'ttfontdata/';
+    const JPGRAPH_DIR = self::MPDF_DIR . 'jpgraph/';
 
     /**
      * Cria a instancia do mpdf
@@ -35,9 +36,13 @@ class MpdfService implements FactoryInterface
         if (!file_exists(self::TTFONTDATA_DIR)) {
             mkdir(self::TTFONTDATA_DIR, 0777, true);
         }
+        if (!file_exists(self::JPGRAPH_DIR)) {
+            mkdir(self::JPGRAPH_DIR, 0777, true);
+        }
 
         define("_MPDF_TEMP_PATH", self::TEMP_DIR);
         define("_MPDF_TTFONTDATAPATH", self::TTFONTDATA_DIR);
+        define("_JPGRAPH_PATH", self::JPGRAPH_DIR);
         return new mPDF();
 
     }
